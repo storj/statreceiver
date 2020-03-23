803bc3bc355f85bbda4223c24634fd2d538e7e09
Isaac Hess <isaachess@gmail.com>
2020-03-23 10:13:52 -0600

    Initial commit

---
+++ cmd/statreceiver/main.go
@@ -0,0 +1,113 @@
+// Copyright (C) 2019 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/BurntSushi/toml"
+	"github.com/spf13/cobra"
+	"github.com/zeebo/errs"
+
+	"storj.io/common/fpath"
+	"storj.io/private/cfgstruct"
+	"storj.io/private/process"
+	"storj.io/statreceiver"
+	"storj.io/statreceiver/luacfg"
+)
+
+// Config is the set of configuration values we care about
+var Config struct {
+	Input            string `default:"" help:"path to configuration file"`
+	DowngradeMetrics string `default:"" help:"path to downgrade metrics toml file"`
+}
+
+func main() {
+	defaultConfDir := fpath.ApplicationDir("storj", "statreceiver")
+
+	cmd := &cobra.Command{
+		Use:   os.Args[0],
+		Short: "stat receiving",
+		RunE:  Main,
+	}
+	defaults := cfgstruct.DefaultsFlag(cmd)
+	process.Bind(cmd, &Config, defaults, cfgstruct.ConfDir(defaultConfDir))
+	cmd.Flags().String("config", filepath.Join(defaultConfDir, "config.yaml"), "path to configuration")
+	process.Exec(cmd)
+}
+
+// Main is the real main method
+func Main(cmd *cobra.Command, args []string) error {
+	var input io.Reader
+	switch Config.Input {
+	case "":
+		return fmt.Errorf("--input path to script is required")
+	case "stdin":
+		input = os.Stdin
+	default:
+		inputFile, err := os.Open(Config.Input)
+		if err != nil {
+			return err
+		}
+		defer func() {
+			if err := inputFile.Close(); err != nil {
+				log.Printf("Failed to close input: %scope", err)
+			}
+		}()
+
+		input = inputFile
+	}
+
+	if Config.DowngradeMetrics == "" {
+		return fmt.Errorf("--downgrade-metrics toml path required")
+	}
+
+	var knownMetrics map[string]string
+	if _, err := toml.DecodeFile(Config.DowngradeMetrics, &knownMetrics); err != nil {
+		return fmt.Errorf("failed to decode toml file: %s", err)
+	}
+
+	scope := luacfg.NewScope()
+	err := errs.Combine(
+		scope.RegisterVal("deliver", statreceiver.Deliver),
+		scope.RegisterVal("filein", statreceiver.NewFileSource),
+		scope.RegisterVal("fileout", statreceiver.NewFileDest),
+		scope.RegisterVal("udpin", statreceiver.NewUDPSource),
+		scope.RegisterVal("udpout", statreceiver.NewUDPDest),
+		scope.RegisterVal("parse", statreceiver.NewParser),
+		scope.RegisterVal("print", statreceiver.NewPrinter),
+		scope.RegisterVal("packetprint", statreceiver.NewPacketPrinter),
+		scope.RegisterVal("pcopy", statreceiver.NewPacketCopier),
+		scope.RegisterVal("mcopy", statreceiver.NewMetricCopier),
+		scope.RegisterVal("pbuf", statreceiver.NewPacketBuffer),
+		scope.RegisterVal("mbuf", statreceiver.NewMetricBuffer),
+		scope.RegisterVal("packetfilter", statreceiver.NewPacketFilter),
+		scope.RegisterVal("headermultivalmatcher", statreceiver.NewHeaderMultiValMatcher),
+		scope.RegisterVal("appfilter", statreceiver.NewApplicationFilter),
+		scope.RegisterVal("instfilter", statreceiver.NewInstanceFilter),
+		scope.RegisterVal("keyfilter", statreceiver.NewKeyFilter),
+		scope.RegisterVal("sanitize", statreceiver.NewSanitizer),
+		scope.RegisterVal("graphite", statreceiver.NewGraphiteDest),
+		scope.RegisterVal("influx", statreceiver.NewInfluxDest),
+		scope.RegisterVal("db", statreceiver.NewDBDest),
+		scope.RegisterVal("pbufprep", statreceiver.NewPacketBufPrep),
+		scope.RegisterVal("mbufprep", statreceiver.NewMetricBufPrep),
+		scope.RegisterVal("downgrade", statreceiver.NewMetricDowngrade(knownMetrics)),
+		scope.RegisterVal("versionsplit", statreceiver.NewVersionSplit),
+	)
+	if err != nil {
+		return err
+	}
+
+	err = scope.Run(input)
+	if err != nil {
+		return err
+	}
+
+	select {}
+}
---
+++ go.mod
@@ -0,0 +1,18 @@
+module storj.io/statreceiver
+
+go 1.13
+
+require (
+	github.com/BurntSushi/toml v0.3.1
+	github.com/Shopify/go-lua v0.0.0-20191113154418-05ce435a9edd
+	github.com/jtolds/go-luar v0.0.0-20200310225017-6fa637b8208b
+	github.com/lib/pq v1.3.0
+	github.com/mattn/go-sqlite3 v2.0.3+incompatible
+	github.com/spf13/cobra v0.0.6
+	github.com/zeebo/admission/v2 v2.0.0
+	github.com/zeebo/admission/v3 v3.0.1
+	github.com/zeebo/errs v1.2.2
+	storj.io/common v0.0.0-20200323134045-2bd4d6e2dd7d
+	storj.io/drpc v0.0.8 // indirect
+	storj.io/private v0.0.0-20200323154727-e555cfbe576d
+)
