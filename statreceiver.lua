7dc99daf24ea07730f18a67ca016c216b1c46116
Ivan Fraixedes <ivan@fraixed.es>
2023-08-17 15:59:44 +0200

    statreceiver.lua: Sync current server config
    Copy the current lua configuraiton from the servers which is running
    statreceiver to the repository.
    
    Change-Id: Ib9dbe569690a75023dc4846f54da95fe616dd45c

--- statreceiver.lua
+++ statreceiver.lua
@@ -43,10 +43,12 @@ influx2_token = os.getenv("INFLUX_2_TOKEN")
 v3_url = string.format("%s/write?db=v3_stats_new&u=%s&p=%s",influx_base, influx_user, influx_pass)
 wp_url = string.format("%s/write?db=v3_stats_new&u=%s&p=%s",influx_wp_base, influx_wp_user, influx_wp_pass)
 inf2_v3_url = string.format("%s/api/v2/write?bucket=v3_stats_new/autogen&org=storj&authorization=%s",influx2_base,influx2_token)
+--webproxy_url = string.format("%s/write?db=webproxy&u=%s&p=%s",influx_base, influx_user, influx_pass)
 
 influx_out_v3 = influx(v3_url)
 influx_out_wp = influx(wp_url)
 influx2_out_v3 = influx(inf2_v3_url)
+--influx_out_webproxy = influx(webproxy_url)
 
 --    mbuf(graphite_out_stefan, mbufsize),
   -- send specific storagenode data to the db
@@ -62,6 +64,7 @@ influx2_out_v3 = influx(inf2_v3_url)
 v3_metric_handlers = mbufprep(mbuf("influx_new", influx_out_v3, mbufsize))
 wp_metric_handlers = mbufprep(mbuf("influx_new", influx_out_wp, mbufsize))
 inf2_v3_metric_handlers = mbufprep(mbuf("influx_new", influx2_out_v3, mbufsize))
+--webproxy_metric_handlers = mbufprep(mbuf("influx_webproxy", influx_out_webproxy, mbufsize))
 
 allowed_instance_id_applications = "(storjscan|orbiter|healthcheck|satellite|retrievability|webproxy|gateway-mt|linksharing|authservice)"
 
@@ -69,8 +72,8 @@ allowed_instance_id_applications = "(storjscan|orbiter|healthcheck|satellite|ret
 metric_parser = parse(zeroinstanceifnot(allowed_instance_id_applications, v3_metric_handlers))
 inf2_metric_parser = parse(zeroinstanceifnot(allowed_instance_id_applications, inf2_v3_metric_handlers))
 wp_metric_parser = parse(wp_metric_handlers)
+---webproxy_metric_parser = parse(webproxy_metric_handlers)
 
-    --packetfilter(".*", "", udpout("localhost:9002")))
     --packetfilter("(storagenode|satellite)-(dev|prod|alphastorj|stagingstorj)", ""))
 
 af = "(storjscan|orbiter|linksharing|gateway-mt|authservice|satellite|retrievability-checker|downloadData|uploadData|healthcheck).*(-alpha|-release|storj|-transfersh)"
@@ -94,13 +97,18 @@ destination = pbufprep(pcopy(
   pbuf(packetfilter(af, "", nil, metric_parser), pbufsize),
 
   -- influx2
-  pbuf(packetfilter(af, "", nil, inf2_metric_parser), pbufsize),
+  -- pbuf(packetfilter(af, "", nil, inf2_metric_parser), pbufsize),
+
+  -- webproxy
+  --pbuf(packetfilter(af_webproxy, "", nil, webproxy_metric_parser), pbufsize),
 
   -- webproxy dedicated
   pbuf(packetfilter(af_webproxy, "", nil, wp_metric_parser), pbufsize),
 
   -- useful local debugging
   pbuf(udpout("localhost:9001"), pbufsize),
+
+  --  pbuf(packetfilter("gateway-mt-release", "", nil, udpout("localhost:9003")), pbufsize),
 
   -- rothko
    pbuf(packetfilter(af_rothko, "", nil, udpout("rothko-internal.storj.tools:9002")), pbufsize)
